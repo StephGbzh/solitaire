fda612c0a1c93168e6e7cfcd977c90f52f597b7e
StephGbzh <StephGbzh@users.noreply.github.com>
2021-08-08 15:33:10 +0200

    Handle move of several cards at once

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const family = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "V", "D", "R"];
-const stock = [];
+let stock = [];
 
 ["♥", "♦", "♣", "♠"].forEach((color) => {
   family.forEach((rank) => {
@@ -8,41 +8,56 @@ const stock = [];
 });
 
 // https://www.w3docs.com/snippets/javascript/how-to-randomize-shuffle-a-javascript-array.html
-function shuffleArray(arr) {
-  arr.sort(() => Math.random() - 0.5);
-}
+const shuffleArray = (arr) => arr.sort(() => Math.random() - 0.5);
 
 shuffleArray(stock);
 shuffleArray(stock);
 shuffleArray(stock);
 
-function dragstart_handler(ev) {
+// https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API
+const dragstart_handler = (ev) => {
   ev.originalEvent.target.setAttribute("id", "beingdragged");
   ev.originalEvent.dataTransfer.dropEffect = "move";
-}
+};
 
-const createCard = (text, shown) =>
-  $("<div>")
+const createCard = (text, shown) => {
+  const card = $("<div>")
     .addClass("card")
-    .addClass(shown ? "face-up" : "face-down")
     .addClass(text.slice(-1)) // extract family
-    .attr("draggable", true)
-    .on("dragstart", dragstart_handler)
-    .css({
-      position: "absolute",
-      textAlign: "right",
-    })
-    .text(text);
+    .append($("<div>").addClass("t").text(text));
 
-const addToPile = (pile, cardText, shown) => {
-  pile.append(createCard(cardText, shown).css({ marginTop: 20 * pile.find(".card").length }));
+  if (shown) {
+    card.addClass("face-up").attr("draggable", true).on("dragstart", dragstart_handler);
+  } else {
+    card.addClass("face-down");
+  }
+
+  return card;
 };
 
-const moveBetweenPiles = (pileTo, card) => {
-  const pile = $(pileTo);
-  pile.append(card.css({ marginTop: 20 * pile.find(".card").length }));
-};
+let talon = [];
 
+$(".stock").click((ev) => {
+  if (stock.length == 0) {
+    stock = talon.reverse();
+    talon = [];
+    $(".talon > .card").remove()
+    $(".stock").removeClass("empty")
+    return
+  }
+  if (stock.length == 1) {
+    $(".stock").addClass("empty")
+  } else {
+    $(".stock").removeClass("empty")
+  }
+  
+  const cardText = stock.pop();
+  talon.push(cardText);
+  const card = createCard(cardText, true);
+  $(".talon").append(card);
+});
+
+// in fact, we store here only the last card on each pile
 const piles = [];
 
 for (let i = 0; i <= 6; i++) {
@@ -51,24 +66,32 @@ for (let i = 0; i <= 6; i++) {
     .attr("ondragover", "dragover_handler(event)");
 }
 
+const addToPile = (column, cardText, shown) => {
+  card = createCard(cardText, shown);
+  piles[column].append(card);
+  piles[column] = card;
+};
+console.log(piles);
+
 for (let line = 0; line <= 6; line++) {
   for (let column = line; column <= 6; column++) {
-    const card = stock.pop();
+    const cardText = stock.pop();
     const shown = line == column;
-    addToPile(piles[column], card, shown);
+    addToPile(column, cardText, shown);
   }
 }
 
 console.log(stock);
 
-function dragover_handler(ev) {
-  ev.preventDefault();
-}
+const dragover_handler = (ev) => ev.preventDefault();
 
-function drop_handler(ev) {
+const drop_handler = (ev) => {
   ev.preventDefault();
-  const card = $("#beingdragged").removeAttr("id");
-  moveBetweenPiles(ev.target.parentNode, card);
-}
-
-const talon = [];
+  const card = $("#beingdragged");
+  const cardParent = card.parent();
+  if (cardParent.hasClass("card") && !cardParent.hasClass("talon")) {
+    cardParent.removeClass("face-down").addClass("face-up").attr("draggable", true).on("dragstart", dragstart_handler);
+  }
+  card.removeAttr("id");
+  $(ev.target).append(card);
+};
